3a5109c56bc90bf8b569e007fa0465d260649cbb
Daniel Robertson <52652357+endail@users.noreply.github.com>
2024-06-26 23:50:36 +1000

    Update generate.py

--- scripts/generate.py
+++ scripts/generate.py
@@ -2,42 +2,63 @@ import json
 import glob
 import subprocess
 
+def resolvedns(endpoint):
+  return subprocess.run(
+    ["nslookup", "-querytype=A", "-retry=0", "-timeout=3", endpoint],
+    capture_output=True,
+    text=True).returncode == 0
+
+def ntpdig(endpoint):
+  result = subprocess.run(
+    ["ntpdig", "-j", "-t 5", endpoint],
+    capture_output=True,
+    text=True,
+    timeout=5)
+  return json.loads(result.stdout)
+
+
+results = []
+
 for file in glob.glob("src/*.json"):
-
-  print("Opening " + file)
-  
-  f = open(file)
-  data = json.load(f)
-  f.close()
-
-  print(data["description"])
+  with open(file) as f: data = json.load(f)
+  listresult = { name: data["name"], description: data["description"], servers: [] }
   
   for server in data["servers"]:
-    print(server["name"])
-
+    srvresult = { name: server["name"], endpoints: [] }
+    
     for endpoint in server["endpoints"]:
-      print("Checking " + endpoint + "...")
-
-      try:
-        result = subprocess.run(["nslookup", endpoint, "1.1.1.1"], capture_output=True, text=True, timeout=5)
-        print("DNS: " + "OK" if result.returncode == 0 else "Fail")
-      except subprocess.TimeoutExpired:
-        print("Timeout")
-        continue
-        
-      if result.returncode != 0:
-        continue
-
-      try:
-        result = subprocess.run(["ntpdig", "-j", "-t 5", endpoint], capture_output=True, text=True, timeout=5)
-        print("ntpdig success?: " + "Yes" if result.returncode == 0 else "No")
-      except subprocess.TimeoutExpired:
-        print("Timeout")
-        continue
+      eptresult = { endpoint: endpoint, dns: False, ntp: None }
       
-      if result.returncode != 0:
-        continue
+      if resolvedns(endpoint):
+        eptresult.dns = True
         
-      ntpout = json.loads(result.stdout)
-      print("Host: %s, IP: %s, Stratum: %s" % (ntpout["host"], ntpout["ip"], ntpout["stratum"]))
-        
+        try:
+          dig = ntpdig(endpoint)
+          eptresult.ntp.stratum = dig.stratum
+          eptresult.ntp.ip = dig.ip
+        except:
+          print("ntpdig failed for " + endpoint)
+      
+      srvresult.endpoints.append(eptresult)
+    listresult.servers.append(srvresult)
+  results.append(srvresult)
+  
+
+# print results
+for list in results:
+  print("List name: " + list.name)
+  print("List description: " + list.description)
+  print()
+  for(srv in list.endpoints):
+    print("\tServer name: " + srv.name)
+    for(ept in srv.endpoints):
+      print("\t\t" + ept.endpoint)
+      print("\t\t\t" + ept.dns)
+      print("\t\t\t" + ept.ip)
+      print("\t\t\t" + ept.stratum)
+      print()
+    print()
+  print()
+
+print("DONE!")
+  
