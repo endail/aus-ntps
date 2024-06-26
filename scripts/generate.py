48f061cf7f9634a8cd047c92e6cd2bce25ab6ef3
Daniel Robertson <52652357+endail@users.noreply.github.com>
2024-06-26 21:55:08 +1000

    Create generate.py

---
+++ scripts/generate.py
@@ -0,0 +1,34 @@
+import json
+import glob
+import subprocess
+
+for file in glob.glob("src/*.json"):
+
+  f = open(file)
+  data = json.load(f)
+  f.close()
+
+  print(data["description"])
+  
+  for server in data["servers"]:
+    print(server["name"])
+
+      for endpoint in server["endpoints"]:
+        print("Checking " + endpoint + "...")
+
+        result = subprocess.run(["nslookup", endpoint, "1.1.1.1"], timeout=5)
+        print("DNS: " + "OK" if result.returncode == 0 else "Fail")
+
+        if result.returncode != 0:
+          continue
+
+        result = subprocess.run(["ntpdig", "-j", "-t 5", endpoint, timeout=5])
+        print("ntpdig success?: " + "Yes" if result.returncode == 0 else "No")
+
+        if result.returncode != 0
+          continue
+        
+        ntpout = json.loads(result.stdout)
+
+        print("Host: %s, IP: %s, Stratum: %s" % (ntpout["host"], ntpout["ip"], ntpout["stratum"))
+        
