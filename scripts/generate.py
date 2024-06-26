2c7e3b30a28ee8d7a959ef476e200991f324427c
Daniel Robertson <52652357+endail@users.noreply.github.com>
2024-06-26 21:59:11 +1000

    Update generate.py

--- scripts/generate.py
+++ scripts/generate.py
@@ -13,22 +13,21 @@ for file in glob.glob("src/*.json"):
   for server in data["servers"]:
     print(server["name"])
 
-      for endpoint in server["endpoints"]:
-        print("Checking " + endpoint + "...")
+    for endpoint in server["endpoints"]:
+      print("Checking " + endpoint + "...")
 
-        result = subprocess.run(["nslookup", endpoint, "1.1.1.1"], timeout=5)
-        print("DNS: " + "OK" if result.returncode == 0 else "Fail")
+      result = subprocess.run(["nslookup", endpoint, "1.1.1.1"], timeout=5)
+      print("DNS: " + "OK" if result.returncode == 0 else "Fail")
 
-        if result.returncode != 0:
-          continue
+      if result.returncode != 0:
+        continue
 
-        result = subprocess.run(["ntpdig", "-j", "-t 5", endpoint, timeout=5])
-        print("ntpdig success?: " + "Yes" if result.returncode == 0 else "No")
+      result = subprocess.run(["ntpdig", "-j", "-t 5", endpoint, timeout=5])
+      print("ntpdig success?: " + "Yes" if result.returncode == 0 else "No")
 
-        if result.returncode != 0
-          continue
+      if result.returncode != 0
+        continue
         
-        ntpout = json.loads(result.stdout)
-
-        print("Host: %s, IP: %s, Stratum: %s" % (ntpout["host"], ntpout["ip"], ntpout["stratum"))
+      ntpout = json.loads(result.stdout)
+      print("Host: %s, IP: %s, Stratum: %s" % (ntpout["host"], ntpout["ip"], ntpout["stratum"))
         
