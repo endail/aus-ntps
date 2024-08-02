14c520b596727f5d8c6035bf611037238ea7c6b1
endail <52652357+endail@users.noreply.github.com>
2024-08-03 01:33:42 +1000

    full run

--- generate.py
+++ generate.py
@@ -1,9 +1,7 @@
 from src.NtpDiscovery import NtpDiscovery, NtpLookupCollection
-import random
 from datetime import datetime, timezone
 
 coll = NtpLookupCollection.fromfile("data/lookups.json")
-coll._data = random.sample(coll._data, 1)
 coll.validate()
 disc = NtpDiscovery(coll)
 report = disc.run()
